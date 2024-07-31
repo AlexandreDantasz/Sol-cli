6096de0a8bfb12f7f3b5d0f05f69a8549b9c57f1
Alexandre Dantas <alexandredantas690@outlook.com>
2024-07-31 17:42:28 -0300

    Adiciona fast i/o no template do código gerado

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ public class Program
         {
             using (FileStream fs = File.Create($"./{args[0]}.cpp"))
             {   
-                string text = "#include <bits/stdc++.h>\n\nusing namespace std;\n\nint main()\n{\n\treturn 0;\n}\n";
+                string text = "#include <bits/stdc++.h>\n\nusing namespace std;\n\nint main()\n{\n\tios_base::sync_with_stdio(false);\n\tcin.tie(NULL);\n\treturn 0;\n}\n";
                 byte[] info = new UTF8Encoding(true).GetBytes(text);
                 fs.Write(info, 0, info.Length);
             }
--- obj/Release/net8.0/sol.cli.AssemblyInfo.cs
+++ obj/Release/net8.0/sol.cli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sol.cli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a6543915b9e01bfc0976a53fbe4bb424c60e26")]
 [assembly: System.Reflection.AssemblyProductAttribute("sol.cli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sol.cli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
